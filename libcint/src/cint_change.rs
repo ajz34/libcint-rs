71feab5156099cd9508197907976e6499921e85c
ajz34 <ajz34@outlook.com>
2025-06-13 16:24:48 +0800

    various with clause

--- libcint/src/cint_change.rs
+++ libcint/src/cint_change.rs
@@ -289,4 +289,263 @@ impl CInt {
     }
 
     /* #endregion */
+
+    /* #region common_origin */
+
+    pub fn get_common_origin(&self) -> [f64; 3] {
+        const PTR_COMMON_ORIG: usize = cint_ffi::PTR_COMMON_ORIG as usize;
+        self.env[PTR_COMMON_ORIG..PTR_COMMON_ORIG + 3].try_into().unwrap()
+    }
+
+    pub fn set_common_origin(&mut self, origin: [f64; 3]) -> &mut Self {
+        const PTR_COMMON_ORIG: usize = cint_ffi::PTR_COMMON_ORIG as usize;
+        self.env[PTR_COMMON_ORIG..PTR_COMMON_ORIG + 3].copy_from_slice(&origin);
+        self
+    }
+
+    pub fn with_common_origin<R>(
+        &mut self,
+        origin: [f64; 3],
+        func: impl FnOnce(&mut Self) -> R,
+    ) -> R {
+        let old_origin = self.get_common_origin();
+        self.set_common_origin(origin);
+        let result = func(self);
+        self.set_common_origin(old_origin);
+        result
+    }
+
+    /* #endregion */
+
+    /* #region rinv_origin */
+
+    pub fn get_rinv_origin(&self) -> [f64; 3] {
+        const PTR_RINV_ORIG: usize = cint_ffi::PTR_RINV_ORIG as usize;
+        self.env[PTR_RINV_ORIG..PTR_RINV_ORIG + 3].try_into().unwrap()
+    }
+
+    pub fn set_rinv_origin(&mut self, origin: [f64; 3]) -> &mut Self {
+        const PTR_RINV_ORIG: usize = cint_ffi::PTR_RINV_ORIG as usize;
+        self.env[PTR_RINV_ORIG..PTR_RINV_ORIG + 3].copy_from_slice(&origin);
+        self
+    }
+
+    pub fn with_rinv_origin<R>(
+        &mut self,
+        origin: [f64; 3],
+        func: impl FnOnce(&mut Self) -> R,
+    ) -> R {
+        let old_origin = self.get_rinv_origin();
+        self.set_rinv_origin(origin);
+        let result = func(self);
+        self.set_rinv_origin(old_origin);
+        result
+    }
+
+    /* #endregion */
+
+    /* #region range_coulomb */
+
+    pub fn get_range_coulomb(&self) -> f64 {
+        const PTR_RANGE_OMEGA: usize = cint_ffi::PTR_RANGE_OMEGA as usize;
+        self.env[PTR_RANGE_OMEGA]
+    }
+
+    pub fn set_range_coulomb(&mut self, range: f64) -> &mut Self {
+        const PTR_RANGE_OMEGA: usize = cint_ffi::PTR_RANGE_OMEGA as usize;
+        self.env[PTR_RANGE_OMEGA] = range;
+        self
+    }
+
+    pub fn with_range_coulomb<R>(&mut self, range: f64, func: impl FnOnce(&mut Self) -> R) -> R {
+        let old_range = self.get_range_coulomb();
+        self.set_range_coulomb(range);
+        let result = func(self);
+        self.set_range_coulomb(old_range);
+        result
+    }
+
+    pub fn omega(&self) -> f64 {
+        self.get_range_coulomb()
+    }
+
+    pub fn set_omega(&mut self, omega: f64) -> &mut Self {
+        self.set_range_coulomb(omega)
+    }
+
+    pub fn with_long_range_coulomb<R>(
+        &mut self,
+        omega: f64,
+        func: impl FnOnce(&mut Self) -> R,
+    ) -> R {
+        self.with_range_coulomb(omega, func)
+    }
+
+    pub fn with_short_range_coulomb<R>(
+        &mut self,
+        omega: f64,
+        func: impl FnOnce(&mut Self) -> R,
+    ) -> R {
+        self.with_range_coulomb(-omega, func)
+    }
+
+    /* #endregion */
+
+    /* #region f12_zeta */
+
+    #[cfg(feature = "with_f12")]
+    pub fn get_f12_zeta(&self) -> f64 {
+        const PTR_F12_ZETA: usize = cint_ffi::PTR_F12_ZETA as usize;
+        self.env[PTR_F12_ZETA]
+    }
+
+    #[cfg(feature = "with_f12")]
+    pub fn set_f12_zeta(&mut self, zeta: f64) -> &mut Self {
+        const PTR_F12_ZETA: usize = cint_ffi::PTR_F12_ZETA as usize;
+        self.env[PTR_F12_ZETA] = zeta;
+        self
+    }
+
+    #[cfg(feature = "with_f12")]
+    pub fn with_f12_zeta<R>(&mut self, zeta: f64, func: impl FnOnce(&mut Self) -> R) -> R {
+        let old_zeta = self.get_f12_zeta();
+        self.set_f12_zeta(zeta);
+        let result = func(self);
+        self.set_f12_zeta(old_zeta);
+        result
+    }
+
+    /* #endregion */
+
+    /* #region set_nuc_mod */
+
+    pub fn set_nuc_mod(&mut self, atm_id: usize, zeta: f64) -> &mut Self {
+        const PTR_ZETA: usize = cint_ffi::PTR_ZETA as usize;
+        const NUC_MOD_OF: usize = cint_ffi::NUC_MOD_OF as usize;
+        const POINT_NUC: c_int = cint_ffi::POINT_NUC as c_int;
+        const GAUSSIAN_NUC: c_int = cint_ffi::GAUSSIAN_NUC as c_int;
+
+        let ptr = self.atm[atm_id][PTR_ZETA] as usize;
+        self.env[ptr] = zeta;
+        self.atm[atm_id][NUC_MOD_OF] = if zeta == 0.0 { POINT_NUC } else { GAUSSIAN_NUC };
+        self
+    }
+
+    /* #endregion */
+
+    /* #region rinv_zeta */
+
+    pub fn get_rinv_zeta(&self) -> f64 {
+        const PTR_RINV_ZETA: usize = cint_ffi::PTR_RINV_ZETA as usize;
+        self.env[PTR_RINV_ZETA]
+    }
+
+    pub fn set_rinv_zeta(&mut self, zeta: f64) -> &mut Self {
+        const PTR_RINV_ZETA: usize = cint_ffi::PTR_RINV_ZETA as usize;
+        self.env[PTR_RINV_ZETA] = zeta;
+        self
+    }
+
+    pub fn with_rinv_zeta<R>(&mut self, zeta: f64, func: impl FnOnce(&mut Self) -> R) -> R {
+        let old_zeta = self.get_rinv_zeta();
+        self.set_rinv_zeta(zeta);
+        let result = func(self);
+        self.set_rinv_zeta(old_zeta);
+        result
+    }
+
+    /* #endregion */
+
+    /* #region rinv_at_nucleus */
+
+    pub fn set_rinv_at_nucleus(&mut self, atm_id: usize) -> &mut Self {
+        const PTR_ZETA: usize = cint_ffi::PTR_ZETA as usize;
+        let zeta = self.env[self.atm[atm_id][PTR_ZETA] as usize];
+        let rinv = self.atom_coord(atm_id);
+        self.set_rinv_origin(rinv);
+        if zeta != 0.0 {
+            self.set_rinv_zeta(zeta);
+        }
+        self
+    }
+
+    pub fn with_rinv_at_nucleus<R>(
+        &mut self,
+        atm_id: usize,
+        func: impl FnOnce(&mut Self) -> R,
+    ) -> R {
+        let old_rinv = self.get_rinv_origin();
+        let old_zeta = self.get_rinv_zeta();
+        self.set_rinv_at_nucleus(atm_id);
+        let result = func(self);
+        self.set_rinv_origin(old_rinv);
+        self.set_rinv_zeta(old_zeta);
+        result
+    }
+
+    /* #endregion */
+
+    /* #region integral_screen */
+
+    pub fn get_integral_screen(&self) -> f64 {
+        const PTR_EXPCUTOFF: usize = cint_ffi::PTR_EXPCUTOFF as usize;
+        self.env[PTR_EXPCUTOFF].exp()
+    }
+
+    pub fn set_integral_screen(&mut self, screen: f64) -> &mut Self {
+        const PTR_EXPCUTOFF: usize = cint_ffi::PTR_EXPCUTOFF as usize;
+        self.env[PTR_EXPCUTOFF] = screen.ln();
+        self
+    }
+
+    pub fn with_integral_screen<R>(&mut self, screen: f64, func: impl FnOnce(&mut Self) -> R) -> R {
+        let old_screen = self.get_integral_screen();
+        self.set_integral_screen(screen);
+        let result = func(self);
+        self.set_integral_screen(old_screen);
+        result
+    }
+
+    /* #endregion */
+
+    /* #region set_geom */
+
+    pub fn get_geom(&self) -> Vec<[f64; 3]> {
+        const PTR_COORD: usize = cint_ffi::PTR_COORD as usize;
+        self.atm
+            .iter()
+            .map(|atm| {
+                let ptr = atm[PTR_COORD] as usize;
+                self.env[ptr..ptr + 3].try_into().unwrap()
+            })
+            .collect()
+    }
+
+    pub fn set_geom(&mut self, coords: &[[f64; 3]]) -> &mut Self {
+        const PTR_COORD: usize = cint_ffi::PTR_COORD as usize;
+
+        if self.natm() != coords.len() {
+            panic!(
+                "Number of coordinates ({}) does not match number of atoms ({})",
+                coords.len(),
+                self.natm()
+            );
+        }
+
+        coords.iter().enumerate().for_each(|(i, coord)| {
+            let ptr = self.atm[i][PTR_COORD] as usize;
+            self.env[ptr..ptr + 3].copy_from_slice(coord);
+        });
+        self
+    }
+
+    pub fn with_geom<R>(&mut self, coords: &[[f64; 3]], func: impl FnOnce(&mut Self) -> R) -> R {
+        let old_geom = self.get_geom();
+        self.set_geom(coords);
+        let result = func(self);
+        self.set_geom(&old_geom);
+        result
+    }
+
+    /* #endregion */
 }
