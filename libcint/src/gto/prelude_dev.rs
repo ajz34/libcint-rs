3cd61293928dc939c0c9ec139ff41f4e7eb7671b
ajz34 <ajz34@outlook.com>
2025-11-20 19:27:30 +0800

    a local version of f64x8

--- libcint/src/gto/prelude_dev.rs
+++ libcint/src/gto/prelude_dev.rs
@@ -4,6 +4,7 @@ pub use core::ffi::c_int;
 pub use core::mem::transmute;
 
 pub const BLKSIZE: usize = 56;
+pub const BLKSIMD: usize = 7;
 // 128s42p21d12f8g6h4i3j
 pub const NCTR_CART: usize = 128;
 
@@ -21,15 +22,23 @@ pub const YY: usize = 3;
 pub const YZ: usize = 4;
 pub const ZZ: usize = 5;
 
+use core::ops::*;
+
 unsafe extern "C" {
     pub fn CINTcommon_fac_sp(l: c_int) -> f64;
 }
 
+/* #region simple f64x8 */
+
+// TODO: use fearless_simd or pulp, they are more heavy but more complete SIMD
+// abstraction crates
+
 #[repr(align(64))]
 #[derive(Clone, Debug, Copy)]
-pub struct BlkF64(pub [f64; BLKSIZE]);
+#[allow(non_camel_case_types)]
+pub struct f64x8(pub [f64; 8]);
 
-impl std::ops::Index<usize> for BlkF64 {
+impl Index<usize> for f64x8 {
     type Output = f64;
     #[inline(always)]
     fn index(&self, index: usize) -> &Self::Output {
@@ -37,7 +46,163 @@ impl std::ops::Index<usize> for BlkF64 {
     }
 }
 
-impl std::ops::IndexMut<usize> for BlkF64 {
+impl IndexMut<usize> for f64x8 {
+    #[inline(always)]
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        &mut self.0[index]
+    }
+}
+
+impl f64x8 {
+    #[inline(always)]
+    pub const fn zero() -> Self {
+        f64x8([0.0f64; 8])
+    }
+
+    /// # Safety
+    ///
+    /// This function returns an uninitialized object. The caller must ensure
+    /// that the returned value is properly initialized before use.
+    #[inline(always)]
+    #[allow(clippy::uninit_assumed_init)]
+    #[allow(invalid_value)]
+    pub const unsafe fn uninit() -> Self {
+        core::mem::MaybeUninit::uninit().assume_init()
+    }
+
+    #[inline(always)]
+    pub const fn splat(val: f64) -> Self {
+        f64x8([val; 8])
+    }
+
+    #[inline(always)]
+    pub const fn fill(&mut self, val: f64) {
+        self.0 = [val; 8];
+    }
+
+    pub fn map<F>(&self, f: F) -> Self
+    where
+        F: Fn(f64) -> f64,
+    {
+        f64x8(self.0.map(f))
+    }
+}
+
+#[duplicate_item(
+    Trait trait_fn;
+    [Add] [add];
+    [Sub] [sub];
+    [Mul] [mul];
+    [Div] [div];
+)]
+mod impl_trait_for_f64x8 {
+    use super::*;
+
+    // simd * simd
+    impl Trait for f64x8 {
+        type Output = Self;
+        #[inline(always)]
+        fn trait_fn(self, rhs: Self) -> Self::Output {
+            f64x8([
+                f64::trait_fn(self.0[0], rhs.0[0]),
+                f64::trait_fn(self.0[1], rhs.0[1]),
+                f64::trait_fn(self.0[2], rhs.0[2]),
+                f64::trait_fn(self.0[3], rhs.0[3]),
+                f64::trait_fn(self.0[4], rhs.0[4]),
+                f64::trait_fn(self.0[5], rhs.0[5]),
+                f64::trait_fn(self.0[6], rhs.0[6]),
+                f64::trait_fn(self.0[7], rhs.0[7]),
+            ])
+        }
+    }
+
+    // simd * scalar
+    impl Trait<f64> for f64x8 {
+        type Output = Self;
+        #[inline(always)]
+        fn trait_fn(self, rhs: f64) -> Self::Output {
+            f64x8([
+                f64::trait_fn(self.0[0], rhs),
+                f64::trait_fn(self.0[1], rhs),
+                f64::trait_fn(self.0[2], rhs),
+                f64::trait_fn(self.0[3], rhs),
+                f64::trait_fn(self.0[4], rhs),
+                f64::trait_fn(self.0[5], rhs),
+                f64::trait_fn(self.0[6], rhs),
+                f64::trait_fn(self.0[7], rhs),
+            ])
+        }
+    }
+
+    // scalar * simd
+    impl Trait<f64x8> for f64 {
+        type Output = f64x8;
+        #[inline(always)]
+        fn trait_fn(self, rhs: f64x8) -> Self::Output {
+            f64x8([
+                f64::trait_fn(self, rhs.0[0]),
+                f64::trait_fn(self, rhs.0[1]),
+                f64::trait_fn(self, rhs.0[2]),
+                f64::trait_fn(self, rhs.0[3]),
+                f64::trait_fn(self, rhs.0[4]),
+                f64::trait_fn(self, rhs.0[5]),
+                f64::trait_fn(self, rhs.0[6]),
+                f64::trait_fn(self, rhs.0[7]),
+            ])
+        }
+    }
+}
+
+#[duplicate_item(
+    Trait trait_fn;
+    [AddAssign] [add_assign];
+    [SubAssign] [sub_assign];
+    [MulAssign] [mul_assign];
+    [DivAssign] [div_assign];
+)]
+impl Trait for f64x8 {
+    #[inline(always)]
+    fn trait_fn(&mut self, rhs: Self) {
+        self.0[0].trait_fn(rhs.0[0]);
+        self.0[1].trait_fn(rhs.0[1]);
+        self.0[2].trait_fn(rhs.0[2]);
+        self.0[3].trait_fn(rhs.0[3]);
+        self.0[4].trait_fn(rhs.0[4]);
+        self.0[5].trait_fn(rhs.0[5]);
+        self.0[6].trait_fn(rhs.0[6]);
+        self.0[7].trait_fn(rhs.0[7]);
+    }
+}
+
+impl f64x8 {
+    #[inline(always)]
+    pub fn fma(&mut self, a: &f64x8, b: &f64x8) {
+        self.0[0] = self.0[0].mul_add(a.0[0], b.0[0]);
+        self.0[1] = self.0[1].mul_add(a.0[1], b.0[1]);
+        self.0[2] = self.0[2].mul_add(a.0[2], b.0[2]);
+        self.0[3] = self.0[3].mul_add(a.0[3], b.0[3]);
+        self.0[4] = self.0[4].mul_add(a.0[4], b.0[4]);
+        self.0[5] = self.0[5].mul_add(a.0[5], b.0[5]);
+        self.0[6] = self.0[6].mul_add(a.0[6], b.0[6]);
+        self.0[7] = self.0[7].mul_add(a.0[7], b.0[7]);
+    }
+}
+
+/* #endregion */
+
+#[repr(align(64))]
+#[derive(Clone, Debug, Copy)]
+pub struct BlkF64(pub [f64; BLKSIZE]);
+
+impl Index<usize> for BlkF64 {
+    type Output = f64;
+    #[inline(always)]
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.0[index]
+    }
+}
+
+impl IndexMut<usize> for BlkF64 {
     #[inline(always)]
     fn index_mut(&mut self, index: usize) -> &mut Self::Output {
         &mut self.0[index]
