22c8153af92e4aa066ec8e6e5e84db7db41d4b48
ajz34 <ajz34@outlook.com>
2025-11-24 13:26:28 +0800

    changes logic of buffer partition

--- libcint/src/gto/grid_ao_drv.rs
+++ libcint/src/gto/grid_ao_drv.rs
@@ -205,9 +205,35 @@ pub fn gto_fill_grid2atm(
     }
 }
 
+pub fn gto_nctr_cart_max(shls_slice: [usize; 2], bas: &[[c_int; BAS_SLOTS as usize]]) -> usize {
+    const ANG_OF: usize = crate::ffi::cint_ffi::ANG_OF as usize;
+    const NCTR_OF: usize = crate::ffi::cint_ffi::NCTR_OF as usize;
+    let [sh0, sh1] = shls_slice;
+    (sh0..sh1)
+        .map(|ish| {
+            let l = bas[ish][ANG_OF] as usize;
+            let nctr = bas[ish][NCTR_OF] as usize;
+            let ncart = (l + 1) * (l + 2) / 2;
+            nctr * ncart
+        })
+        .max()
+        .unwrap_or(0)
+}
+
+pub fn gto_nctr_max(shls_slice: [usize; 2], bas: &[[c_int; BAS_SLOTS as usize]]) -> usize {
+    const NCTR_OF: usize = crate::ffi::cint_ffi::NCTR_OF as usize;
+    let [sh0, sh1] = shls_slice;
+    (sh0..sh1).map(|ish| bas[ish][NCTR_OF] as usize).max().unwrap_or(0)
+}
+
+pub fn gto_nprim_max(shls_slice: [usize; 2], bas: &[[c_int; BAS_SLOTS as usize]]) -> usize {
+    const NPRIM_OF: usize = crate::ffi::cint_ffi::NPRIM_OF as usize;
+    let [sh0, sh1] = shls_slice;
+    (sh0..sh1).map(|ish| bas[ish][NPRIM_OF] as usize).max().unwrap_or(0)
+}
+
 #[allow(clippy::too_many_arguments)]
 pub fn gto_eval_iter<const CART: bool>(
-    // evaluator
     evaluator: &dyn GTOEvalAPI,
     // arguments
     ao: &mut [f64], // (ncomp, nao, ngrid)
@@ -229,7 +255,6 @@ pub fn gto_eval_iter<const CART: bool>(
     env: &[f64],
 ) {
     const ATOM_OF: usize = crate::ffi::cint_ffi::ATOM_OF as usize;
-    const NCTR_MAX: usize = crate::ffi::cint_ffi::NCTR_MAX as usize;
     const NPRIM_OF: usize = crate::ffi::cint_ffi::NPRIM_OF as usize;
     const NCTR_OF: usize = crate::ffi::cint_ffi::NCTR_OF as usize;
     const ANG_OF: usize = crate::ffi::cint_ffi::ANG_OF as usize;
@@ -237,15 +262,20 @@ pub fn gto_eval_iter<const CART: bool>(
     const PTR_COEFF: usize = crate::ffi::cint_ffi::PTR_COEFF as usize;
 
     let ncomp = evaluator.ncomp();
+    let nctr_cart_max = gto_nctr_cart_max(shls_slice, bas);
+    let nctr_max = gto_nctr_max(shls_slice, bas);
+    let nprim_max = gto_nprim_max(shls_slice, bas);
+
     let [sh0, sh1] = shls_slice;
     let [atm0, atm1] = [bas[sh0][ATOM_OF] as usize, bas[sh1 - 1][ATOM_OF] as usize + 1];
     let atm_count = atm1 - atm0;
     let nbuf_grid2atm = atm_count * 3;
-    let nbuf_eprim = 2 * NCTR_MAX;
+    let nbuf_eprim = 2 * nctr_max.max(nprim_max);
+    let nbuf_gto = ncomp * nctr_cart_max;
     let (grid2atm, buf) = buf.split_at_mut(nbuf_grid2atm);
     let (eprim, buf) = buf.split_at_mut(nbuf_eprim);
-    let (gto_cart, buf) = buf.split_at_mut(NCOMP_MAX * NCTR_CART);
-    let (gto_sph, _) = buf.split_at_mut(NCOMP_MAX * NCTR_CART);
+    let (gto_cart, buf) = buf.split_at_mut(nbuf_gto);
+    let (gto_sph, _) = buf.split_at_mut(nbuf_gto);
     let bgrid = (ngrid - igrid).min(BLKSIZE);
 
     let grid2atm = unsafe { grid2atm.as_chunks_unchecked_mut::<3>() };
@@ -296,8 +326,6 @@ pub fn gto_eval_loop<const CART: bool>(
     bas: &[[c_int; BAS_SLOTS as usize]],
     env: &[f64],
 ) {
-    const NCTR_MAX: usize = crate::ffi::cint_ffi::NCTR_MAX as usize;
-
     let ncomp = evaluator.ncomp();
     let [sh0, sh1] = shls_slice;
     let nao = ao_loc[sh1] - ao_loc[sh0];
@@ -315,9 +343,15 @@ pub fn gto_eval_loop<const CART: bool>(
     // eprim or ectr: 2 * nctr
     // gto_cart: ncomp_max * nctr_cart
     // gto_sph: ncomp_max * nctr_cart
-    const NCACHE: usize = 2 * NCTR_MAX + 2 * NCOMP_MAX * NCTR_CART + 3;
+    let nctr_cart_max = gto_nctr_cart_max(shls_slice, bas);
+    let nctr_max = gto_nctr_max(shls_slice, bas);
+    let nprim_max = gto_nprim_max(shls_slice, bas);
+    let nbuf_grid2atm = 3;
+    let nbuf_eprim = 2 * nctr_max.max(nprim_max);
+    let nbuf_gto = 2 * ncomp * nctr_cart_max;
+    let ncache = nbuf_eprim + nbuf_gto + nbuf_grid2atm;
     let nthreads = rayon::current_num_threads();
-    let thread_cache = (0..nthreads).map(|_| unsafe { [f64blk::uninit(); NCACHE] }).collect_vec();
+    let thread_cache = (0..nthreads).map(|_| unsafe { vec![f64blk::uninit(); ncache] }).collect_vec();
 
     (0..nblk * nshblk).into_par_iter().for_each(|niter| {
         let ishblk = niter / nblk;
